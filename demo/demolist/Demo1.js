a35762b1f6a230269c705ee20577225e291f0e42
ranyanchuan <ranyanchuan@yimeijian.cn>
2019-07-01 22:33:50 +0800

    demo1

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -6,15 +6,45 @@
  */
 
 import React, { Component } from 'react';
-import Temp from '../../src/index';
+import { Button, Icon } from 'tinper-bee';
+// 项目中引入方式
+// import AcExcelReader from 'ac-excel-reader';
+// demo 中引入方式
+import AcExcelReader from '../../src/index';
+
+
 class Demo1 extends Component {
-    render () {
-        return (
-            <div className="demoPadding">
-                <Temp >tools测试代码</Temp>
-            </div>
-        )
-    }
+  render() {
+    // excel 列名hash 对照
+    const colKeyHash = {
+      order: '序号',
+      level: '程度',
+      component: '组件',
+      solve: '解决人',
+      recorder: '记录人',
+      desc: '问题描述',
+    };
+
+    return (
+      <div className="demoPadding">
+        <AcExcelReader
+
+          getJson={(data) => { // 获取返回json
+            console.log(data);
+          }}
+
+          getArray={(data) => { // 获取返回数组
+            console.log(data);
+          }}
+
+          colKeyHash={colKeyHash}
+
+        >
+          <Button shape="border">上传excel</Button>
+        </AcExcelReader>
+      </div>
+    );
+  }
 }
 
 export default Demo1;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,103 @@
-import Temp from './temp';
-export default Temp;
+/* eslint-disable no-restricted-syntax,no-prototype-builtins,react/prop-types,react/destructuring-assignment,no-shadow */
+import React, { Component } from 'react';
+
+import XLSX from 'xlsx';
+import './index.less';
+
+
+class AcExcelReader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  onImportExcel = (file) => {
+    // 获取上传的文件对象
+    const { files } = file.target;
+    // 通过FileReader对象读取文件
+    const fileReader = new FileReader();
+    // 以二进制方式打开文件
+    fileReader.readAsBinaryString(files[0]);
+
+    fileReader.onload = (event) => {
+
+      try {
+        const { result } = event.target;
+        // 以二进制流方式读取得到整份excel表格对象
+        const workbook = XLSX.read(result, { type: 'binary' });
+        let data = []; // 存储获取到的数据
+        // 遍历每张工作表进行读取（这里默认只读取第一张表）
+        for (const sheet in workbook.Sheets) {
+          if (workbook.Sheets.hasOwnProperty(sheet)) {
+            // 利用 sheet_to_json 方法将 excel 转成 json 数据
+            data = data.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet]));
+            // break; // 如果只取第一张表，就取消注释这行
+          }
+        }
+
+        let jsonData = data;
+        const { colKeyHash } = this.props;
+
+        // json 可以转换
+        if (colKeyHash) {
+          jsonData = data.map((row) => {
+            const result = {};
+            for (const item in row) {
+              // 是否修改key
+              let key = item;
+              for (const colKey in colKeyHash) {
+                if (colKeyHash[colKey] === item) {
+                  key = colKey;
+                  break;
+                }
+              }
+              result[key] = row[item];
+            }
+            return result;
+          });
+        }
+
+
+        const array = data.map((row) => {
+          const result = [];
+          for (const item in row) {
+            result.push(row[item]);
+          }
+          return result;
+        });
+
+        // json
+        this.props.getJson(jsonData);
+        // array
+        this.props.getArray(array);
+      } catch (e) {
+        // 这里可以抛出文件类型错误不正确的相关提示
+        console.log('文件类型不正确');
+      }
+    };
+  };
+
+
+  onClickUpload = (event) => {
+    const input = event.currentTarget.children[0];
+    input.click();
+  };
+
+
+  render() {
+    return (
+      <span onClick={this.onClickUpload}>
+        <input
+          type="file"
+          accept=".xlsx, .xls"
+          style={{ display: 'none' }}
+          onChange={this.onImportExcel}
+        />
+        <span>{this.props.children}</span>
+      </span>
+    );
+  }
+}
+
+export default AcExcelReader;
